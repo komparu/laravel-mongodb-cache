9e1f4d2946f0700af9260b348a9174df4f188344
Kosta <kest.bodnya@gmail.com>
2016-08-18 09:31:02 +0200

    dummy tagged store file

---
+++ src/Komparu/Mongodb/Cache/MongodbTaggedStore.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Komparu\Mongodb\Cache;
+
+
+class MongodbTaggedStore extends TaggableStore implements StoreInterface {
+    public function get($key){}
+
+    public function put($key, $value, $minutes){}
+
+    public function increment($key, $value = 1){}
+
+    public function decrement($key, $value = 1){}
+
+    public function forever($key, $value){}
+
+    public function forget($key){}
+
+    public function flush(){}
+
+    public function getPrefix(){}
+}
