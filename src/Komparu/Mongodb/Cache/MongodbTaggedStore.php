07faa59f1ef5127b2c3656fa248589d2150ea1ad
Kosta <kest.bodnya@gmail.com>
2016-08-18 16:28:16 +0200

    usages of interfaces and abstract classes

--- src/Komparu/Mongodb/Cache/MongodbTaggedStore.php
+++ src/Komparu/Mongodb/Cache/MongodbTaggedStore.php
@@ -3,6 +3,9 @@
 namespace Komparu\Mongodb\Cache;
 
 
+use Illuminate\Cache\StoreInterface;
+use Illuminate\Cache\TaggableStore;
+
 class MongodbTaggedStore extends TaggableStore implements StoreInterface {
     public function get($key){}
 
